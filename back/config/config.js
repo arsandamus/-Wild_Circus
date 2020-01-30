adc6b333a5493dd48bc06a2f435733c11547dd6d
Cyril Carral <cyril.carral85@gmail.com>
2020-01-30 15:27:22 +0100

    config back done

---
+++ back/config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config(process.cwd(), '.env')
+
+const mysql = require('mysql')
+const db_name = ''
+const db_port = 3306 //8889 if you use MAMP Server
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}) 
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log(`😎😎 -- MySql is connected on ${db_name}'s database on port ${db_port} -- 😎😎`);
+    } else {
+        console.log("-- 👎 👎  -- Error connecting MySql : -- 👎 👎 -- ", err);
+    }
+});
+
+module.exports = connection
